539308423f47648c3dd2036ab0f58f4f979f6b7a
Vitor Oliveira <vitor.oliveira@digitalscutum.com.br>
2024-06-20 15:19:54 -0300

    Correção método de input

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+//    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
+//    useJUnitPlatform()
 }
--- src/main/java/ceub/CodeGenerator.java
+++ src/main/java/ceub/CodeGenerator.java
@@ -1,19 +1,30 @@
 package ceub;
 import ceub.statements.CompositeStatement;
 
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CodeGenerator {
     public static void main(String[] args) {
-        String input = "G n\n" +
-                "= i 2\n" +
-                "% a n i\n" +
-                "W i < n {\n" +
-                " I a = 0 = i n\n" +
-                " + i i 1\n" +
-                " % a n i\n" +
-                "}\n" +
-                "I a = 0 P 0\n" +
-                "I a # 0 P 1\n";
-        Parser parser = new Parser(input);
+        boolean i = true;
+        List<String> codes = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o codigo: (Pressione 0 para sair)");
+        while (i){
+            String input = sc.nextLine();
+            if (input.equals("0")){
+                i = false;
+            }else{
+                codes.add(input);
+            }
+        }
+        StringBuilder input = new StringBuilder();
+        for (String line : codes){
+            input.append(line).append("\n");
+        }
+        Parser parser = new Parser(input.toString());
         CompositeStatement program = parser.parse();
         System.out.println("#include <stdio.h>\nint main() {\nint a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;\nchar str[512];");
         program.generateCCode();
